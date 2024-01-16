4688acb8bc918f6503d06b9ad0f753f5bd563bea
Dan Mirman <55460807+dmirman@users.noreply.github.com>
2024-01-16 11:39:37 +0000

    updated example data for lec01

--- msmr_lectures/msmr_lec01_liver.R
+++ msmr_lectures/msmr_lec01_liver.R
@@ -10,7 +10,29 @@ library(lmerTest)
 
 # load and inspect the data
 load("./data/caff_age.rda")
-
+summary(caff_age)
+head(caff_age)
+head(caff_age_multiday)
 # exploratory plots
+ggplot(caff_age, aes(Age, RT, fill=Condition)) +
+  geom_boxplot()
+
+ggplot(caff_age_multiday, aes(Age, RT, fill=Condition)) +
+  geom_boxplot()
 
 # fit the model(s)
+# m <- lmer(RT ~ Condition*Age +
+#             (1 +  Condition | Participant),
+#           data = caff_age, REML=F)
+m <- lmer(RT ~ Condition*Age +
+            (1 +  Condition | Participant),
+          contrasts = list(Condition = contr.sum,
+                           Age = contr.sum),
+          data = caff_age_multiday, REML=F)
+summary(m)
+
+m_ols <- lm(RT ~ Condition*Age,
+            contrasts = list(Condition = contr.sum,
+                             Age = contr.sum),
+            data = caff_age_multiday)
+summary(m_ols)
