de33acc3a2f4252a59206af13571f6b232a80dbc
Dan Mirman <55460807+dmirman@users.noreply.github.com>
2024-01-27 10:28:40 +0000

    live R scripts from week 2

--- msmr_lectures/msmr_lec02-2_liver.R
+++ msmr_lectures/msmr_lec02-2_liver.R
@@ -3,6 +3,7 @@ library(tidyverse)
 library(lme4)
 library(lmerTest)
 library(effects)
+library(broom.mixed)
 
 # Part 2: Visual search - Longitudinal data analysis is not just for longitudinal data
 
@@ -11,7 +12,30 @@ load("./data/VisualSearchEx.rda")
 str(VisualSearchEx)
 
 # plot the data
+ggplot(VisualSearchEx, aes(Set.Size, RT, color=Group)) +
+  stat_summary(fun.data = mean_se, geom="pointrange")
 
 # fit the models
+m <- lmer(RT ~ Set.Size + 
+            (Set.Size | Participant),
+          data=VisualSearchEx, REML=F)
+
+m_g <- lmer(RT ~ Set.Size + Group +
+            (Set.Size | Participant),
+          data=VisualSearchEx, REML=F)
+
+m_full <- lmer(RT ~ Set.Size * Group + 
+            (Set.Size | Participant),
+          data=VisualSearchEx, REML=F)
+
+anova(m, m_g, m_full)
+
+summary(m_full)
+
 
 # plot model fit
+summary(augment(m_full))
+ggplot(augment(m_full), aes(Set.Size, RT, color=Group)) +
+  stat_summary(fun.data = mean_se, geom="pointrange") +
+  stat_summary(aes(y = .fitted), fun=mean, geom="line")
+
