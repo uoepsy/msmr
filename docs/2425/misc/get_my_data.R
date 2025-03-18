2a174374f34bbfa82a52731789b872962b1e98b9
Josiah King <josiahpjking@gmail.com>
2025-03-18 10:56:33 +0000

    save out data to env

--- docs/2425/misc/get_my_data.R
+++ docs/2425/misc/get_my_data.R
@@ -2,4 +2,7 @@ get_my_data <- function(exam_num){
   tryCatch(
     suppressWarnings(load(url(paste0("https://uoepsy.github.io/msmr/2425/misc/",toupper(exam_num),".RData")))),
     error=function(e) cat("No Data Found. Are you sure you used the correct exam number?\nExam numbers can be found on your student card, and take the form of the letter B followed by 6 numbers, for example get_my_data(\"B123456\")\nIf you cannot make this work with your exam number, please use get_my_data(\"UNKNOWN\")"))
+  
+  cbtmodes <<- cbtmodes
+  locus <<- locus
 }
